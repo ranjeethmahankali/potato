3db9126e07d97589e2775a661af394b96c8e296c
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2023-02-06 23:54:25 -0500

    Improve the design and use inkscape to generate texture

--- genTexture.py
+++ genTexture.py
@@ -1,9 +1,9 @@
 """Generate the png assets required for this project."""
-import cairosvg
 from os import path
 from PIL import Image
 from argparse import ArgumentParser
 import numpy as np
+import subprocess
 
 
 def generateTexture():
@@ -17,7 +17,7 @@ def generateTexture():
     src = path.join(root, 'pieces.svg')
     pngpath = path.join(dest, 'pieces.png')
     print("Saving the texture to {} ...".format(pngpath))
-    cairosvg.svg2png(url=src, write_to=pngpath)
+    subprocess.run(['inkscape', src, '-o', pngpath])
     arr = np.array(Image.open(pngpath))
     assert arr.shape[2] == 4
     arr = np.flip(arr, axis=0)
