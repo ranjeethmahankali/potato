51809a93b22ecdc829bb1bacebe83dd5d63ee9df
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2023-02-09 00:15:11 -0500

    Start adding support for commands

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include <Board.h>
+#include <Command.h>
 #include <GLUtil.h>
 #include <View.h>
 #include <chrono>
@@ -20,31 +21,16 @@ static void cmdLoop()
       running = false;
       continue;
     }
-    std::cout << "Received command: " << input << std::endl;
+    command::run(input.begin(), input.end());
   }
 };
 
-static void advance(const Board& b)
-{
-  view::set(b);
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-}
-
 int main(int argc, char** argv)
 {
+  command::init();
   Board       b;
   std::thread cmdThread(cmdLoop);
   view::start();
-  advance(b);
-  advance(b.move({4, 6}, {4, 4}));
-  advance(b.move({4, 1}, {4, 3}));
-  advance(b.move({6, 7}, {5, 5}));
-  advance(b.move({1, 0}, {2, 2}));
-  advance(b.move({3, 6}, {3, 4}));
-  advance(b.move({4, 3}, {3, 4}));
-  advance(b.move({5, 5}, {3, 4}));
-  advance(b.move({2, 2}, {3, 4}));
-  advance(b.move({3, 7}, {3, 4}));
   cmdThread.join();
   view::stop();
   return 0;
