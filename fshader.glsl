8678c33540b44931a02fe6fa7b52f399c72637f3
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2023-02-14 21:39:59 -0500

    Misc changes

--- fshader.glsl
+++ fshader.glsl
@@ -9,10 +9,13 @@ uniform sampler2D pieces;
 void main()
 {
   if (ColorOrTex.z == -1.f) {
+    // This means this triangle represents a piece on a square that is empty.
     FragColor = vec4(0., 0., 0., 0.);
   } else if (ColorOrTex.z == 2.f) {
+    // This means this triangle represents a piece.
     FragColor = texture(pieces, ColorOrTex.xy);
   } else {
+    // This means this triangle represents a square on the board.
     FragColor = vec4(ColorOrTex.rgb, 1.0);
   }
 }
